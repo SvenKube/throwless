8ac44bf0bff599661f9b1d285c8df55a73d84cef
Sven Kube <git@sven-kube.de>
2019-09-02 14:49:29 +0200

    feat: add option type

---
+++ src/option.ts
@@ -0,0 +1,175 @@
+import {Result, ok, err} from "./result";
+
+export const some = <T>(value: T): Option<T> => new Some(value);
+export const none = <T>(): Option<T> => new None();
+
+export interface Option<T> {
+    isSome(): boolean;
+    isNone(): boolean;
+
+    expect(error: any): T;
+    unwrap(): T;
+    unwrapOr(value: T): T;
+    unwrapOrElse(fn: () => T): T;
+
+    map<U>(fn: (value: T) => U): Option<U>;
+    mapOr<U>(value: U, fn: (value: T) => U): U;
+    mapOrElse<U>(fnMap: (value: T) => U, fnOrElse: () => U): U;
+
+    okOr<E>(error: E): Result<T, E>;
+    okOrElse<E>(fn: () => E): Result<T, E>;
+
+    and<U>(optb: Option<U>): Option<U>;
+    andThen<U>(fn: (value: T) => Option<U>): Option<U>;
+
+    filter(fn: (value: T) => boolean): Option<T>;
+
+    or(optb: Option<T>): Option<T>;
+    orElse(fn: () => Option<T>): Option<T>;
+
+    xor(optb: Option<T>): Option<T>;
+}
+
+export class Some<T> implements Option<T> {
+    private value: T;
+
+    constructor(value: T){
+        this.value = value;
+    }
+
+    isSome(): boolean {
+        return true;
+    }
+    isNone(): boolean {
+        return false;
+    }
+
+    expect(_error: any): T {
+        return this.value;
+    }
+
+    unwrap(): T {
+        return this.value;
+    }
+
+    unwrapOr(_value: T): T {
+        return this.value;
+    }
+
+    unwrapOrElse(_fn: () => T): T {
+        return this.value;
+    }
+
+    map<U>(fn: (value: T) => U): Option<U> {
+        return new Some(fn(this.value));
+    }
+
+    mapOr<U>(_value: U, fn: (value: T) => U): U {
+        return fn(this.value);
+    }
+
+    mapOrElse<U>(fnMap: (value: T) => U, _fnOrElse: () => U): U {
+        return fnMap(this.value);
+    }
+
+    okOr<E>(_error: E): Result<T, E> {
+        return ok(this.value);
+    }
+
+    okOrElse<E>(_fn: () => E): Result<T, E> {
+        return ok(this.value);
+    }
+
+    and<U>(optb: Option<U>): Option<U> {
+        return optb;
+    }
+
+    andThen<U>(fn: (value: T) => Option<U>): Option<U> {
+        return fn(this.value);
+    }
+
+    filter(fn: (value: T) => boolean): Option<T> {
+        // @ts-ignore
+        return fn(this.value) ? new Some(this.value) : new None<T>();
+    }
+
+    or(_optb: Option<T>): Option<T> {
+        return new Some(this.value);
+    }
+
+    orElse(_fn: () => Option<T>): Option<T> {
+        return new Some(this.value);
+    }
+
+    xor(optb: Option<T>): Option<T> {
+        return optb.isNone() ? this : new None();
+    }
+}
+
+export class None<T> implements Option<T> {
+    isSome(): boolean {
+        return false;
+    }
+
+    isNone(): boolean {
+        return true;
+    }
+
+    expect(error: any): T {
+        throw error;
+    }
+
+    unwrap(): T {
+        throw Error();
+    }
+    unwrapOr(value: T): T {
+        return value;
+    }
+
+    unwrapOrElse(fn: () => T): T {
+        return fn();
+    }
+
+    map<U>(_fn: (value: T) => U): Option<U> {
+        return new None();
+    }
+
+    mapOr<U>(value: U, _fn: (value: T) => U): U {
+        return value;
+    }
+
+    mapOrElse<U>(_fnMap: (value: T) => U, fnOrElse: () => U): U {
+        return fnOrElse();
+    }
+
+    okOr<E>(error: E): Result<T, E> {
+        return err(error);
+    }
+
+    okOrElse<E>(fn: () => E): Result<T, E> {
+        return err(fn());
+    }
+
+    and<U>(_optb: Option<U>): Option<U> {
+        return new None();
+    }
+    andThen<U>(_fn: (value: T) => Option<U>): Option<U> {
+        return new None();
+    }
+
+    filter(_fn: (value: T) => boolean): Option<T> {
+        return new None();
+    }
+
+    or(optb: Option<T>): Option<T> {
+        return optb;
+    }
+
+    orElse(fn: () => Option<T>): Option<T> {
+        return fn();
+    }
+
+    xor(optb: Option<T>): Option<T> {
+        return optb.isSome() ? optb : new None();
+    }
+}
---
+++ src/result.ts
@@ -0,0 +1,184 @@
+import {Option, some, none} from "./option";
+
+export const ok = <T, E>(value: T): Result<T, E> => new Ok(value);
+export const err = <T, E>(error: E): Result<T, E> => new Err(error);
+
+export interface Result<T, E> {
+    isErr(): boolean;
+    isOk(): boolean;
+
+    ok(): Option<T>;
+    err(): Option<E>;
+
+    map<U>(fn: (value: T) => U): Result<U, E>;
+    mapErr<F>(fn: (error: E) => F): Result<T, F>;
+
+    mapOrElse<U>(fnMap: (value: T) => U, fnOrElse: (error: E) => U): U;
+
+    and<U>(res: Result<U, E>): Result<U, E>;
+    andThen<U>(fn: (value: T) => Result<U, E>): Result<U, E>;
+
+    or<F>(res: Result<T, F>): Result<T, F>;
+    orElse<F>(fn: (error: E) =>  Result<T, F>): Result<T, F>;
+
+    expectErr(error: any): E;
+    expect(error: any): T;
+
+    unwrap(): T;
+    unwrapErr(): E;
+
+    unwrapOr(value: T): T;
+    unwrapOrElse(fn: (error: E) => T): T;
+}
+
+export class Ok<T, E> implements Result<T, E> {
+    private readonly value: T;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+
+    isErr(): boolean {
+        return false;
+    }
+
+    isOk(): boolean {
+        return true;
+    }
+
+    ok(): Option<T> {
+        return some(this.value);
+    }
+
+    err(): Option<E> {
+        return none();
+    }
+
+    unwrap(): T {
+        return this.value;
+    }
+
+    unwrapErr(): E {
+        throw this.value;
+    }
+
+    expectErr(error: any): E {
+        throw error;
+    }
+
+    expect(_error: any): T {
+        return this.value;
+    }
+
+    unwrapOr(_value: T): T {
+        return this.value;
+    }
+
+    unwrapOrElse(_fn: (error: E) => T): T {
+        return this.value;
+    }
+
+    or<F>(_res: Result<T, F>): Result<T, F> {
+        return ok(this.value);
+    }
+
+    orElse<F>(_fn: (error: E) => Result<T, F>): Result<T, F> {
+        return ok(this.value);
+    }
+
+    and<U>(res: Result<U, E>): Result<U, E> {
+        return res;
+    }
+
+    andThen<U>(fn: (value: T) => Result<U, E>): Result<U, E> {
+        return fn(this.value);
+    }
+
+    map<U>(fn: (value: T) => U): Result<U, E> {
+        return ok(fn(this.value));
+    }
+
+    mapErr<F>(_fn: (error: E) => F): Result<T, F> {
+        return ok(this.value);
+    }
+
+    mapOrElse<U>(fnMap: (value: T) => U, fnOrElse: (error: E) => U): U {
+        return this.map(fnMap).unwrapOrElse(fnOrElse);
+    }
+}
+
+export class Err<T, E> implements Result<T, E> {
+    private readonly error: E;
+
+    constructor(error: E) {
+        this.error = error;
+    }
+
+    isErr(): boolean {
+        return true;
+    }
+
+    isOk(): boolean {
+        return false;
+    }
+
+    ok(): Option<T> {
+        return none();
+    }
+
+    err(): Option<E> {
+        return some(this.error);
+    }
+
+    unwrap(): T {
+        throw this.error;
+    }
+
+    unwrapErr(): E {
+        return this.error;
+    }
+
+    expect(error: any): T {
+        throw error;
+    }
+
+    expectErr(_error: any): E {
+        return this.error;
+    }
+
+    unwrapOr(value: T): T {
+        return value;
+    }
+
+    unwrapOrElse(fn: (error: E) => T): T {
+        return fn(this.error);
+    }
+
+    or<F>(res: Result<T, F>): Result<T, F> {
+        return res;
+    }
+
+    orElse<F>(fn: (error: E) => Result<T, F>): Result<T, F> {
+        return fn(this.error);
+    }
+
+    and<U>(_res: Result<U, E>): Result<U, E> {
+        return err(this.error);
+    }
+
+    andThen<U>(_fn: (value: T) => Result<U, E>): Result<U, E> {
+        return err(this.error);
+    }
+
+    map<U>(_fn: (value: T) => U): Result<U, E> {
+        return err(this.error);
+    }
+
+    mapErr<F>(fn: (error: E) => F): Result<T, F> {
+        return err(fn(this.error));
+    }
+
+    mapOrElse<U>(fnMap: (value: T) => U, fnOrElse: (error: E) => U): U {
+        return this.map(fnMap).unwrapOrElse(fnOrElse);
+    }
+}
