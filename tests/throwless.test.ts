b5e71ed3c6649664228911f33c471eb61392fb89
Sven Kube <git@sven-kube.de>
2019-09-03 10:32:14 +0200

    test: add tests for throwless wrapping

--- tests/throwless.test.ts
+++ tests/throwless.test.ts
@@ -1,4 +1,4 @@
-import {Result, throwlessAsync} from "../src";
+import {Result, throwless, throwlessAsync} from "../src";
 
 describe("Tests for throwlessAsync wrapping", () => {
 
@@ -28,6 +28,42 @@ describe("Tests for throwlessAsync wrapping", () => {
 
         expect(result.isErr()).toBeTruthy();
         expect(result.unwrapErr()).toBeUndefined();
+    });
+
+});
+
+describe("Tests for throwless wrapping", () => {
+
+    it("Returns Ok if fn returns value", () => {
+        const result: Result<number, Error> = throwless(() => 773);
+
+        expect(result.isOk()).toBeTruthy();
+        expect(result.unwrap()).toBe(773);
+    });
+
+    it("Returns Ok if fn returns void", () => {
+        const result: Result<void, Error> = throwless(() => {});
+
+        expect(result.isOk()).toBeTruthy();
+        expect(result.unwrap()).toBeUndefined();
+    });
+
+    it("Returns Err if fn throws an error", () => {
+        const result: Result<number, Error> = throwless(() => {
+            throw new Error("Some Error");
+        });
+
+        expect(result.isErr()).toBeTruthy();
+        expect(result.unwrapErr()).toEqual(new Error("Some Error"));
+    });
+
+    it("Returns Err if fn throws", () => {
+        const result: Result<number, Error> = throwless(() => {
+            throw "";
+        });
+
+        expect(result.isErr()).toBeTruthy();
+        expect(result.unwrapErr()).toEqual("");
     });
 
 });
